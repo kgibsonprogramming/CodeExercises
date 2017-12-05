c616a8a3e8916d55154ae098a22a0c1676564aa2
Kyle <kgibsonprogramming@gmail.com>
2017-12-05 14:03:15 -0500

    Add project files.

---
+++ Code Challenges/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestWord("fun&!! time"));
+            Console.WriteLine(LongestWord("I love dogs"));
+            Console.ReadLine();
+        }
+
+        public static string LongestWord(string sens)
+        {
+            int wordLength = 0;
+            string longestWord = "";
+            string[] inputSplit;
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in sens)
+            {
+                if (!char.IsPunctuation(c))
+                {
+                    sb.Append(c);
+                }
+            }
+            inputSplit = sb.ToString().Split(' ');
+
+            foreach (string s in inputSplit)
+            {
+                if (s.Length > wordLength)
+                {
+                    longestWord = s;
+                    wordLength = s.Length;
+                }
+            }
+
+            return "The longest word is " + longestWord + " at " + wordLength + " characters.";
+        }
+    }
+}
